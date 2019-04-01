f5784011f561dcea9aa94838aeefb4f0b358cd3c
dave woodall <github@davewoodall.com>
2019-04-01 16:10:09 -0400

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Afterburners
+  class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot
+      g.fixtures false
+      g.view_specs false
+      g.helper_specs false
+      g.controller_specs false
+      g.routing_specs false
+      g.assets false
+      g.helper false
+    end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
